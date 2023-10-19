31df7e9afa167bb5a58d70eb57c27f77adc4fe4c
ELAND\zhongqilin <z0925955648@gmail.com>
2023-10-19 11:20:15 +0800

    update sql

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ require('dotenv').config();
 
 const express = require('express');
 const line = require('@line/bot-sdk');
+const mysql = require('mysql2/promise');
 
 // create LINE SDK config from env variables
 const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET,
 };
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
 // create LINE SDK client
 const client = new line.Client(config);
@@ -24,56 +35,57 @@ app.get('/healthcheck', (req, res) => {
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.post('/callback', line.middleware(config), (req, res) => {
-  Promise
-      .all(req.body.events.map(handleEvent))
-      .then((result) => res.json(result))
-      .catch((err) => {
-        console.error(err);
-        // 回應 LINE 用戶一個錯誤訊息
-        const errorMessage = {
-          type: 'text',
-          text: '伺服器忙碌中，請稍後重試'
-        };
-        return client.replyMessage(req.body.events[0].replyToken, errorMessage)
-            .then(() => {
-              res.status(500).end();
-            });
-      });
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result))
+        .catch((err) => {
+            console.error(err);
+            // 回應 LINE 用戶一個錯誤訊息
+            const errorMessage = {
+                type: 'text',
+                text: '伺服器忙碌中，請稍後重試'
+            };
+            return client.replyMessage(req.body.events[0].replyToken, errorMessage)
+                .then(() => {
+                    res.status(500).end();
+                });
+        });
 });
 
 // event handler
 async function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
-  const profile = await client.getProfile(event.source.userId);
-  console.log(profile.displayName);
-  console.log(profile.userId);
-  console.log(profile.pictureUrl);
-  console.log(profile.statusMessage);
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        // ignore non-text-message event
+        return Promise.resolve(null);
+    }
+    const profile = await client.getProfile(event.source.userId);
+    console.log(profile.displayName);
+    console.log(profile.userId);
+    console.log(profile.pictureUrl);
+    console.log(profile.statusMessage);
 
-  // 這裡，我們直接回應用戶的訊息，不透過 OpenAI
-  let echo = '';
-  // 做一個檢查用戶 id 與資料庫是否 type 已經有 乘客或司機 ，撈出那個用戶做+-動作
-  if (event.message.text === '我是乘客') {
-    echo = { type: 'text', text: `${profile.displayName} ，我已經將您切換為 乘客 !` };
-  } else if (event.message.text === '我是司機') {
-    echo = { type: 'text', text: `${profile.displayName} ，我已經將您切換為 司機 !` };
-  } else {
-      echo = { type: 'text', text: `嗨~ ${profile.displayName} ，我重複一次你的問題: ${event.message.text}` };
-  }
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
+    let echo = '';
+    // 做一個檢查用戶 id 與資料庫是否 type 已經有 乘客或司機 ，撈出那個用戶做+-動作
+    if (event.message.text === '我是乘客') {
+        echo = {type: 'text', text: `${profile.displayName} ，我已經將您切換為 乘客 !`};
+    } else if (event.message.text === '我是司機') {
+        echo = {type: 'text', text: `${profile.displayName} ，我已經將您切換為 司機 !`};
+
+        const [rows, fields] = await pool.execute('INSERT INTO drivers (user_id, display_name, type) VALUES (?, ?, ?)', [profile.userId, profile.displayName, '司機']);
+    } else {
+        echo = {type: 'text', text: `嗨~ ${profile.displayName} ，我重複一次你的問題: ${event.message.text}`};
+    }
+    // use reply API
+    return client.replyMessage(event.replyToken, echo);
 }
 
 // listen on port
 const port = process.env.PORT || 3000;
 // 預設的錯誤處理器
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('伺服器忙碌中，請稍後重試');
+    console.error(err.stack);
+    res.status(500).send('伺服器忙碌中，請稍後重試');
 });
 app.listen(port, () => {
-  console.log(`listening on ${port}`);
+    console.log(`listening on ${port}`);
 });
