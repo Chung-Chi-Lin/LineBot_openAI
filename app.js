2d35f72029160d715d0c9dd567a797c781c90b1d
ELAND\zhongqilin <z0925955648@gmail.com>
2023-10-30 16:53:55 +0800

    change DB

--- app.js
+++ app.js
@@ -2,24 +2,33 @@ require('dotenv').config();
 
 const express = require('express');
 const line = require('@line/bot-sdk');
-const mysql = require('mysql2/promise');
-
+// const mysql = require('mysql2/promise');
+const sql = require('mssql');
 // create LINE、SQL SDK config from env variables
 const config = {
 	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
 	channelSecret: process.env.CHANNEL_SECRET,
 };
 
-const pool = mysql.createPool({
-	host: process.env.DB_HOST,
+// const pool = mysql.createPool({
+// 	host: process.env.DB_HOST,
+// 	user: process.env.DB_USER,
+// 	password: process.env.DB_PASS,
+// 	database: process.env.DB_NAME,
+// 	waitForConnections: true,
+// 	connectionLimit: 10,
+// 	queueLimit: 0,
+// });
+
+const pool = new sql.ConnectionPool({
+	server: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASS,
 	database: process.env.DB_NAME,
-	waitForConnections: true,
 	connectionLimit: 10,
-	queueLimit: 0,
 });
 
+pool.connect();
 // create LINE SDK client
 const client = new line.Client(config);
 
